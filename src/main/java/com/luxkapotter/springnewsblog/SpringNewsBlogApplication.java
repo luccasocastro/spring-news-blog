658bb9f6efd95f5348699842939db6dfe9c0807b
Luccas Souza <emaildocastro.adm01@gmail.com>
2023-10-11 13:53:24 -0300

    chore: setting up the project

---
+++ src/main/java/com/luxkapotter/springnewsblog/SpringNewsBlogApplication.java
@@ -0,0 +1,13 @@
+package com.luxkapotter.springnewsblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNewsBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringNewsBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luxkapotter/springnewsblog/SpringNewsBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luxkapotter.springnewsblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNewsBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
